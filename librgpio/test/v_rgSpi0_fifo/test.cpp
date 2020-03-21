bca4b735d267a0905a8b35ae44528f3cdc1733bd
William Hudson <wahudson>
2020-03-21 10:06:30 -0600

    Test:  v_rgSpi0_fifo/  Refactor for new rgSpi0 register objects.     No functional change.  Still a checkpoint.

--- librgpio/test/v_rgSpi0_fifo/test.cpp
+++ librgpio/test/v_rgSpi0_fifo/test.cpp
@@ -10,7 +10,8 @@
 // Provide external configuration:
 //    % rgpio fsel --mode=Alt0  11 10 9 8 7
 //    GPIO[11:7] == {SCLK, MOSI, MISO, CE0_n, CE1_n}
-//    All GPIO pins unconnected.  Jumper MISO (pin 9) to MOSI (pin 10).
+//    Jumper MISO (Gpio[9], pin 21) to MOSI (Gpio[10], pin 19).
+//    All other GPIO pins unconnected.
 //--------------------------------------------------------------------------
 
 #include <iostream>	// std::cerr
@@ -47,28 +48,28 @@ rgSpi0			Tx   ( &Bx );		// test object
 
 
 //--------------------------------------------------------------------------
-//## write_Fifo() with RunActive_1=0
+//## Fifo.write() with RunActive_1=0
 //--------------------------------------------------------------------------
 // Writing Fifo with RunActive_1=0 modifies part of CntlStat.
 
   CASE( "10", "reset all registers" );
     try {
 	Tx.init_put_reset();
-	Tx.put_CntlStat(   0x00000000 );
-	Tx.put_ClearRxTxFifo_2(     3 );	// one-shot, reads as 0
-	Tx.put_ReadEnable_1(        0 );	// allow MOSI drive
-	Tx.put_ClkDiv(     0x000000ff );	// make it not too fast
-	CHECKX(            0x00000030, Tx.get_CntlStat() );
-	CHECKX(            0x000000ff, Tx.get_ClkDiv()   );
-	CHECKX(            0x00000000, Tx.get_DmaLen()   );
-	CHECKX(            0x00000001, Tx.get_Lossi()    );
-	CHECKX(            0x30201020, Tx.get_DmaReq()   );
+	Tx.CntlStat.put(   0x00000000 );
+	Tx.CntlStat.put_ClearRxTxFifo_2(     3 );	// one-shot, reads as 0
+	Tx.CntlStat.put_ReadEnable_1(        0 );	// allow MOSI drive
+	Tx.ClkDiv.put(     0x000000ff );	// make it not too fast
+	CHECKX(            0x00000030, Tx.CntlStat.get() );
+	CHECKX(            0x000000ff, Tx.ClkDiv.get()   );
+	CHECKX(            0x00000000, Tx.DmaLen.get()   );
+	CHECKX(            0x00000001, Tx.Lossi.get()    );
+	CHECKX(            0x30201020, Tx.DmaReq.get()   );
 	Tx.push_regs();
-	CHECKX(            0x00040000, Tx.read_CntlStat() );
-	CHECKX(            0x000000ff, Tx.read_ClkDiv()   );
-	CHECKX(            0x00000000, Tx.read_DmaLen()   );
-	CHECKX(            0x00000001, Tx.read_Lossi()    );
-	CHECKX(            0x30201020, Tx.read_DmaReq()   );
+	CHECKX(            0x00040000, Tx.CntlStat.read() );
+	CHECKX(            0x000000ff, Tx.ClkDiv.read()   );
+	CHECKX(            0x00000000, Tx.DmaLen.read()   );
+	CHECKX(            0x00000001, Tx.Lossi.read()    );
+	CHECKX(            0x30201020, Tx.DmaReq.read()   );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -78,13 +79,13 @@ rgSpi0			Tx   ( &Bx );		// test object
 // Successful write/read fifo in loop back.
   CASE( "11a", "setup RunActive_1=1" );
     try {
-	Tx.write_CntlStat( 0x00000000 );
+	Tx.CntlStat.write( 0x00000000 );
 	Tx.grab_regs();
-	CHECKX(            0x00040000, Tx.get_CntlStat() );
-	Tx.put_RunActive_1(         1 );
-	CHECKX(            0x00040080, Tx.get_CntlStat() );
+	CHECKX(            0x00040000, Tx.CntlStat.get() );
+	Tx.CntlStat.put_RunActive_1(         1 );
+	CHECKX(            0x00040080, Tx.CntlStat.get() );
 	Tx.push_regs();
-	CHECKX(            0x00050080, Tx.read_CntlStat() );
+	CHECKX(            0x00050080, Tx.CntlStat.read() );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -93,40 +94,40 @@ rgSpi0			Tx   ( &Bx );		// test object
   CASE( "11b", "verify bits after set RunActive_1=1" );
     try {
 	Tx.grab_regs();
-	CHECKX(            0x00050080, Tx.get_CntlStat() );
-	CHECK(                      0, Tx.get_LossiWord_1()     );
-	CHECK(                      0, Tx.get_LossiDmaEn_1()    );
-	CHECK(                      0, Tx.get_CsPolarity_3()    );
-	CHECK(                      0, Tx.get_RxFullStop_1()    );
-	CHECK(                      0, Tx.get_RxHalf_1()        );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      0, Tx.get_RxHasData_1()     );
-	CHECK(                      1, Tx.get_TxEmpty_1()       );
-	CHECK(                      0, Tx.get_LossiEnable_1()   );
-	CHECK(                      0, Tx.get_ReadEnable_1()    );
-	CHECK(                      0, Tx.get_DmaEndCs_1()      );
-	CHECK(                      0, Tx.get_IrqRxHalf_1()     );
-	CHECK(                      0, Tx.get_IrqTxEmpty_1()    );
-	CHECK(                      0, Tx.get_DmaEnable_1()     );
-	CHECK(                      1, Tx.get_RunActive_1()     );
-	CHECK(                      0, Tx.get_CsPolarity_1()    );
-	CHECK(                      0, Tx.get_ClearRxTxFifo_2() );
-	CHECK(                      0, Tx.get_ClockPolarity_1() );
-	CHECK(                      0, Tx.get_ClockPhase_1()    );
-	CHECK(                      0, Tx.get_ChipSelectN_2()   );
+	CHECKX(            0x00050080, Tx.CntlStat.get() );
+	CHECK(                      0, Tx.CntlStat.get_LossiWord_1()     );
+	CHECK(                      0, Tx.CntlStat.get_LossiDmaEn_1()    );
+	CHECK(                      0, Tx.CntlStat.get_CsPolarity_3()    );
+	CHECK(                      0, Tx.CntlStat.get_RxFullStop_1()    );
+	CHECK(                      0, Tx.CntlStat.get_RxHalf_1()        );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      0, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      1, Tx.CntlStat.get_TxEmpty_1()       );
+	CHECK(                      0, Tx.CntlStat.get_LossiEnable_1()   );
+	CHECK(                      0, Tx.CntlStat.get_ReadEnable_1()    );
+	CHECK(                      0, Tx.CntlStat.get_DmaEndCs_1()      );
+	CHECK(                      0, Tx.CntlStat.get_IrqRxHalf_1()     );
+	CHECK(                      0, Tx.CntlStat.get_IrqTxEmpty_1()    );
+	CHECK(                      0, Tx.CntlStat.get_DmaEnable_1()     );
+	CHECK(                      1, Tx.CntlStat.get_RunActive_1()     );
+	CHECK(                      0, Tx.CntlStat.get_CsPolarity_1()    );
+	CHECK(                      0, Tx.CntlStat.get_ClearRxTxFifo_2() );
+	CHECK(                      0, Tx.CntlStat.get_ClockPolarity_1() );
+	CHECK(                      0, Tx.CntlStat.get_ClockPhase_1()    );
+	CHECK(                      0, Tx.CntlStat.get_ChipSelectN_2()   );
 	// Note TxEmpty_1 is active only when RunActive_1=1.
     }
     catch (...) {
 	FAIL( "unexpected exception" );
     }
 
-  CASE( "11c", "write_Fifo()" );
+  CASE( "11c", "Fifo.write()" );
     try {
-	Tx.write_Fifo(     0x00000088 );
-	Tx.write_Fifo(     0x00000077 );
-	Tx.write_Fifo(     0x00000099 );
+	Tx.Fifo.write(     0x00000088 );
+	Tx.Fifo.write(     0x00000077 );
+	Tx.Fifo.write(     0x00000099 );
 	Tx.grab_regs();
-	CHECKX(            0x00040080, Tx.get_CntlStat() );
+	CHECKX(            0x00040080, Tx.CntlStat.get() );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -137,30 +138,30 @@ rgSpi0			Tx   ( &Bx );		// test object
 	int		ii;
 	for ( ii=0;  ii<=1000;  ii++ )
 	{
-	    Tx.grab_CntlStat();
-	    if ( 1 == Tx.get_TxEmpty_1() ) { break; }
+	    Tx.CntlStat.grab();
+	    if ( 1 == Tx.CntlStat.get_TxEmpty_1() ) { break; }
 	}
 	cout << "11d:  wait ii= " << ii <<endl;
 	Tx.grab_regs();
-	CHECKX(            0x00070080, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      1, Tx.get_RxHasData_1()     );
-	CHECK(                      1, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x00070080, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      1, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      1, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
     }
 
-  CASE( "11e", "read_Fifo()" );
+  CASE( "11e", "Fifo.read()" );
     try {
-	CHECKX(            0x00000088, Tx.read_Fifo() );
-	CHECKX(            0x00000077, Tx.read_Fifo() );
-	CHECKX(            0x00000099, Tx.read_Fifo() );
+	CHECKX(            0x00000088, Tx.Fifo.read() );
+	CHECKX(            0x00000077, Tx.Fifo.read() );
+	CHECKX(            0x00000099, Tx.Fifo.read() );
 	Tx.grab_regs();
-	CHECKX(            0x00050080, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      0, Tx.get_RxHasData_1()     );
-	CHECK(                      1, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x00050080, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      0, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      1, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -174,22 +175,22 @@ rgSpi0			Tx   ( &Bx );		// test object
   CASE( "20", "setup registers" );
     try {
 	Tx.init_put_reset();
-	Tx.put_CntlStat(   0x00000000 );
-	Tx.put_ClearRxTxFifo_2(     3 );	// one-shot, reads as 0
-	Tx.put_ReadEnable_1(        0 );	// allow MOSI drive
-	Tx.put_RunActive_1(         1 );
-	Tx.put_ClkDiv(     0x000000ff );	// make it not too fast
-	CHECKX(            0x000000b0, Tx.get_CntlStat() );
-	CHECKX(            0x000000ff, Tx.get_ClkDiv()   );
-	CHECKX(            0x00000000, Tx.get_DmaLen()   );
-	CHECKX(            0x00000001, Tx.get_Lossi()    );
-	CHECKX(            0x30201020, Tx.get_DmaReq()   );
+	Tx.CntlStat.put(   0x00000000 );
+	Tx.CntlStat.put_ClearRxTxFifo_2(     3 );	// one-shot, reads as 0
+	Tx.CntlStat.put_ReadEnable_1(        0 );	// allow MOSI drive
+	Tx.CntlStat.put_RunActive_1(         1 );
+	Tx.ClkDiv.put(     0x000000ff );	// make it not too fast
+	CHECKX(            0x000000b0, Tx.CntlStat.get() );
+	CHECKX(            0x000000ff, Tx.ClkDiv.get()   );
+	CHECKX(            0x00000000, Tx.DmaLen.get()   );
+	CHECKX(            0x00000001, Tx.Lossi.get()    );
+	CHECKX(            0x30201020, Tx.DmaReq.get()   );
 	Tx.push_regs();
-	CHECKX(            0x00050080, Tx.read_CntlStat() );
-	CHECKX(            0x000000ff, Tx.read_ClkDiv()   );
-	CHECKX(            0x00000000, Tx.read_DmaLen()   );
-	CHECKX(            0x00000001, Tx.read_Lossi()    );
-	CHECKX(            0x30201020, Tx.read_DmaReq()   );
+	CHECKX(            0x00050080, Tx.CntlStat.read() );
+	CHECKX(            0x000000ff, Tx.ClkDiv.read()   );
+	CHECKX(            0x00000000, Tx.DmaLen.read()   );
+	CHECKX(            0x00000001, Tx.Lossi.read()    );
+	CHECKX(            0x30201020, Tx.DmaReq.read()   );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -200,17 +201,17 @@ rgSpi0			Tx   ( &Bx );		// test object
 	uint32_t	ii;
 	for ( ii=0;  ii<=1000;  ii++ )
 	{
-	    Tx.write_Fifo(     ii );
-	    Tx.grab_CntlStat();
-//	    if ( 1 == Tx.get_RxFullStop_1() ) { break; }
-	    if ( 0 == Tx.get_TxHasSpace_1() ) { break; }
+	    Tx.Fifo.write(     ii );
+	    Tx.CntlStat.grab();
+//	    if ( 1 == Tx.CntlStat.get_RxFullStop_1() ) { break; }
+	    if ( 0 == Tx.CntlStat.get_TxHasSpace_1() ) { break; }
 	}
 	cout << "21a:  Tx full ii= " << ii <<endl;
 	Tx.grab_regs();
-	CHECKX(            0x00060080, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      1, Tx.get_RxHasData_1()     );
-	CHECK(                      0, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x00060080, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      1, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      0, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -221,17 +222,17 @@ rgSpi0			Tx   ( &Bx );		// test object
 	int		ii;
 	for ( ii=0;  ii<=100000;  ii++ )
 	{
-	    Tx.grab_CntlStat();
-	    if ( 1 == Tx.get_RxFullStop_1() ) { break; }
+	    Tx.CntlStat.grab();
+	    if ( 1 == Tx.CntlStat.get_RxFullStop_1() ) { break; }
 	}
 	cout << "21b:  wait ii= " << ii <<endl;
 	Tx.grab_regs();
-	CHECKX(            0x001e0080, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_RxFullStop_1()    );
-	CHECK(                      1, Tx.get_RxHalf_1()        );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      1, Tx.get_RxHasData_1()     );
-	CHECK(                      0, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x001e0080, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_RxFullStop_1()    );
+	CHECK(                      1, Tx.CntlStat.get_RxHalf_1()        );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      1, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      0, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -240,11 +241,11 @@ rgSpi0			Tx   ( &Bx );		// test object
   CASE( "21c", "stop Tx" );
     try {
 	Tx.grab_regs();
-	Tx.put_RunActive_1(         0 );
-	Tx.put_ClearRxTxFifo_2(     1 );	// clear Tx
-	CHECKX(            0x001e0010, Tx.get_CntlStat() );
+	Tx.CntlStat.put_RunActive_1(         0 );
+	Tx.CntlStat.put_ClearRxTxFifo_2(     1 );	// clear Tx
+	CHECKX(            0x001e0010, Tx.CntlStat.get() );
 	Tx.push_regs();
-	CHECKX(            0x001e0010, Tx.get_CntlStat() );
+	CHECKX(            0x001e0010, Tx.CntlStat.get() );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -256,19 +257,19 @@ rgSpi0			Tx   ( &Bx );		// test object
 	uint32_t	rv;
 	for ( ii=1;  ii<=1000;  ii++ )
 	{
-	    rv = Tx.read_Fifo();
+	    rv = Tx.Fifo.read();
 //	    cout << "21d:  Rx[" << ii << "]= " << rv <<endl;	// see data
-	    Tx.grab_CntlStat();
-	    if ( 0 == Tx.get_RxHasData_1() ) { break; }
+	    Tx.CntlStat.grab();
+	    if ( 0 == Tx.CntlStat.get_RxHasData_1() ) { break; }
 	}
 	CHECK(                     64, ii );	// Rx is 64 entries
 	CHECK(                     63, rv );	// last entry received
 //	cout << "21d:  Rx empty ii= " << ii <<endl;
 	Tx.grab_regs();
-	CHECKX(            0x00040000, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      0, Tx.get_RxHasData_1()     );
-	CHECK(                      0, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x00040000, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      0, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      0, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
@@ -277,10 +278,10 @@ rgSpi0			Tx   ( &Bx );		// test object
   CASE( "21e", "check status" );
     try {
 	Tx.grab_regs();
-	CHECKX(            0x00040000, Tx.get_CntlStat() );
-	CHECK(                      1, Tx.get_TxHasSpace_1()    );
-	CHECK(                      0, Tx.get_RxHasData_1()     );
-	CHECK(                      0, Tx.get_TxEmpty_1()       );
+	CHECKX(            0x00040000, Tx.CntlStat.get() );
+	CHECK(                      1, Tx.CntlStat.get_TxHasSpace_1()    );
+	CHECK(                      0, Tx.CntlStat.get_RxHasData_1()     );
+	CHECK(                      0, Tx.CntlStat.get_TxEmpty_1()       );
     }
     catch (...) {
 	FAIL( "unexpected exception" );
