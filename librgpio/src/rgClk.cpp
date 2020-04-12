0b79d01d5161f2d119bd28b172db5a94e9f31217
William Hudson <wahudson>
2020-04-12 13:02:31 -0600

    rgClk:  Merged base class rgClkMan:: into the rgClk.* files.     No functional change, both classes exist unchanged.     The rgClkMan.* files are deleted.     This parallels the other features, and puts all the clock manager     code together making it easier to see.

--- librgpio/src/rgClk.cpp
+++ librgpio/src/rgClk.cpp
@@ -19,6 +19,200 @@ using namespace std;
 #include "rgAddrMap.h"
 #include "rgClk.h"
 
+// This file contains two classes declared in rgClk.h:
+//    rgClkMan::  Base Class
+//    rgClk::     Derived Class
+
+//==========================================================================
+// rgClkMan::  Base Class
+//==========================================================================
+
+/*
+* Constructor.
+*    No address initialization here, do in the derived class.
+*/
+rgClkMan::rgClkMan()
+{
+    WaitTime_ns = 0;		// no nanosleep
+    WaitCount   = 10000;	// about 10x margin
+    BusyCount   = 0;
+}
+
+
+//--------------------------------------------------------------------------
+// Object state operations
+//--------------------------------------------------------------------------
+
+/*
+* Initialize object control/status registers to the power-on reset state.
+*    Intended for use with the put_*() field accessor functions, and can be
+*    called multiple times.
+*    Virtually all fields are zero, or are the inactive state.
+*    Hardware registers are unchanged.
+*/
+void
+rgClkMan::init_put_reset()
+{
+    Cntl.put(     0x00000000 );
+    Divr.put(     0x00000000 );
+}
+
+
+/*
+* Read hardware registers into the object.
+*/
+void
+rgClkMan::grab_regs()
+{
+    Cntl.grab();
+    Divr.grab();
+}
+
+
+/*
+* Write hardware registers from the object.
+*/
+void
+rgClkMan::push_regs()
+{
+    Cntl.push();
+    Divr.push();
+}
+
+
+/*
+* Apply passwd, then write hardware registers from the object.
+*/
+void
+rgClkMan::apply_regs()
+{
+    Cntl.apply();
+    Divr.apply();
+}
+
+
+//--------------------------------------------------------------------------
+// Special functions
+//--------------------------------------------------------------------------
+
+/*
+* Apply registers safely, first disabling clock and waiting for not busy.
+*    The safe sequence is:
+*        Disable clock if it was running, then wait for not busy.
+*        Apply registers without simultaneous enable.
+*        Apply Enable_1 if requested.
+*    Register values are NOT written if still busy after wait timeout.
+*    Both Cntl and Divr are applied with password fields set to 0x5a.
+*    Use accessors to configure WaitTime_ns and WaitCount.
+*    See wait_while_busy().
+* Note:  In general, changing field values when busy can result in a corrupted
+*    state where the clock output is the wrong frequency or hung.
+* call:
+*    apply_nicely()
+* return:
+*    ()  = status:  0= success
+*                   1= wait on busy timeout, clock is disabled
+*/
+bool
+rgClkMan::apply_nicely()
+{
+    bool		busy = 0;
+    rgClk_Cntl		cx  ( this->Cntl );	// construct copy
+
+    cx.grab();				// update register
+
+    if ( cx.get_Enable_1() ) {
+	cx.put_Enable_1( 0 );
+	cx.apply();			// disable clock
+    }
+
+    busy = wait_while_busy();		// check Busy_1 and set BusyCount
+
+    if ( ! busy ) {
+	if ( Cntl.get_Enable_1() ) {
+	    Cntl.put_Enable_1( 0 );	// apply registers without enable
+	    Cntl.apply();
+	    Divr.apply();
+
+	    Cntl.put_Enable_1( 1 );	// then enable
+	    Cntl.apply();
+	}
+	else {
+	    Cntl.apply();
+	    Divr.apply();
+	}
+    }
+
+    return  busy;
+}
+
+
+/*
+* Wait until not Busy or time limit reached.
+*    Does not disable the clock, will time out if still enabled.
+*    Cntl object and hardware registers are unchanged.
+*    Intrinsic loop iteration time is ~310 ns, dominated by the Cntl register
+*    read time.
+* call:
+*    wait_while_busy()
+*    WaitCount   = limit number of wait cycles, 0= no wait
+*    WaitTime_ns = nanoseconds to delay each cycle, 0= none
+* return:
+*    ()        = busy status, 0= success clock stopped, 1= still busy
+*    BusyCount = number of wait cycles until Busy_1=0 or limit reached.
+*/
+bool
+rgClkMan::wait_while_busy()
+{
+    rgClk_Cntl		cx  ( this->Cntl );	// construct copy
+    bool		busy;
+
+    BusyCount = 0;
+    cx.grab();
+    busy = cx.get_Busy_1();
+
+    while ( busy && (BusyCount < WaitCount) )
+    {
+	BusyCount++;		// count number of wait periods
+
+	if ( WaitTime_ns ) {
+	    //#!! nanosleep()
+	}
+
+	cx.grab();
+	busy = cx.get_Busy_1();
+    }
+
+    return  busy;
+}
+
+
+//--------------------------------------------------------------------------
+// Debug
+//--------------------------------------------------------------------------
+
+/*
+* Show debug output.
+* call:
+*    self.show_debug( cout )
+*/
+void
+rgClkMan::show_debug( std::ostream& sout )
+{
+    sout.fill('0');
+    sout <<hex
+	 << "Clk.Cntl= 0x"  <<setw(8) << Cntl.get() <<endl
+	 << "Clk.Divr= 0x"  <<setw(8) << Divr.get() <<endl
+	 ;
+
+    sout <<dec;
+    sout.fill(' ');
+}
+
+
+//==========================================================================
+// rgClk::  Derived Class
+//==========================================================================
 
 /*
 * Constructor.
--- librgpio/src/rgClk.h
+++ librgpio/src/rgClk.h
@@ -3,11 +3,117 @@
 #ifndef rgClk_P
 #define rgClk_P
 
-#include "rgClkMan.h"		// base class
+#include "rgRegister.h"
 
 //--------------------------------------------------------------------------
 // rGPIO  rgClk - Clock Manager enumerated class
 //--------------------------------------------------------------------------
+//    Note:  A function defined within the declaration of a class is inline
+//    by default.
+
+class rgClk_Cntl : public rgRegister {
+  public:
+
+    inline
+    void	apply()			{ put_Passwd_8();  push(); }
+
+    inline
+    uint32_t	get_Passwd_8()        { return  get_field( 24, 0xff       ); }
+    void	put_Passwd_8( uint32_t v )    { put_field( 24, 0xff, v    ); }
+    void	put_Passwd_8()                { put_field( 24, 0xff, 0x5a ); }
+
+    inline
+    uint32_t	get_Mash_2()             { return  get_field(  9, 0x3    ); }
+    void	put_Mash_2( uint32_t v )         { put_field(  9, 0x3, v ); }
+
+    inline
+    uint32_t	get_Flip_1()             { return  get_field(  8, 0x1    ); }
+    void	put_Flip_1( uint32_t v )         { put_field(  8, 0x1, v ); }
+
+    inline
+    uint32_t	get_Busy_1()             { return  get_field(  7, 0x1    ); }
+    void	put_Busy_1( uint32_t v )         { put_field(  7, 0x1, v ); }
+
+    inline
+    uint32_t	get_Kill_1()             { return  get_field(  5, 0x1    ); }
+    void	put_Kill_1( uint32_t v )         { put_field(  5, 0x1, v ); }
+
+    inline
+    uint32_t	get_Enable_1()           { return  get_field(  4, 0x1    ); }
+    void	put_Enable_1( uint32_t v )       { put_field(  4, 0x1, v ); }
+
+    inline
+    uint32_t	get_Source_4()           { return  get_field(  0, 0xf    ); }
+    void	put_Source_4( uint32_t v )       { put_field(  0, 0xf, v ); }
+};
+
+class rgClk_Divr : public rgRegister {
+  public:
+
+    inline
+    void	apply()			{ put_Passwd_8();  push(); }
+
+    inline
+    uint32_t	get_Passwd_8()        { return  get_field( 24, 0xff       ); }
+    void	put_Passwd_8( uint32_t v )    { put_field( 24, 0xff, v    ); }
+    void	put_Passwd_8()                { put_field( 24, 0xff, 0x5a ); }
+
+    inline
+    uint32_t	get_DivI_12()           { return  get_field( 12, 0xfff    ); }
+    void	put_DivI_12( uint32_t v )       { put_field( 12, 0xfff, v ); }
+
+    inline
+    uint32_t	get_DivF_12()           { return  get_field(  0, 0xfff    ); }
+    void	put_DivF_12( uint32_t v )       { put_field(  0, 0xfff, v ); }
+};
+
+
+/*
+* Base Class.
+*    Has registers, but no address initialization.
+*    Intended for deriving a specific clock feature class.
+*    Has no feature address members to avoid confusion.
+*/
+class rgClkMan {
+  public:
+				// Register data
+    rgClk_Cntl		Cntl;		// Control
+    rgClk_Divr		Divr;		// Divider
+
+  private:
+    long		WaitTime_ns;	// nanoseconds delay, 0= none
+    int			WaitCount;	// limit wait cycles, 0= no wait
+    int			BusyCount;	// number of cycles waited
+
+  public:
+    rgClkMan();			// constructor
+
+		// Object state operations
+    void		init_put_reset();
+
+    void		grab_regs();
+    void		push_regs();
+    void		apply_regs();		// with default password
+
+		// Special functions
+
+    bool		apply_nicely();		// apply all registers safely
+
+    bool		wait_while_busy();
+
+    inline int		wait_time_ns()		{ return WaitTime_ns; }
+    inline void		wait_time_ns( int ns )	{ WaitTime_ns = ns; }
+
+    inline int		wait_count_n()		{ return WaitCount; }
+    inline void		wait_count_n( int n )	{ WaitCount = n; }
+
+    inline int		get_busy_count()	{ return BusyCount; }
+
+		// Test/Debug accessors
+
+    void		show_debug( std::ostream&  sout );
+};
+
 
 class rgClk : public rgClkMan {
   public:
@@ -54,7 +160,6 @@ class rgClk : public rgClkMan {
 		// Test/Debug accessors
 
     inline uint32_t	get_bcm_address() { return FeatureAddr; }
-
 };
 
 #endif
