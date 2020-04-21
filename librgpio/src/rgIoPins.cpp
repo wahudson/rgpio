bad0437e8af672afce73404e7db1f11048e1a273
William Hudson <wahudson>
2020-04-21 17:34:13 -0600

    rgIoPins:  IO Pin Registers class - object registers.  Initial design.

---
+++ librgpio/src/rgIoPins.cpp
@@ -0,0 +1,116 @@
+// 2020-04-19  William A. Hudson
+
+// rGPIO  Io Pin Registers.
+//
+// See:  BCM2835 ARM Peripherals (2012)
+//      p.89-101  6.  General Purpose I/O (GPIO)
+//
+//--------------------------------------------------------------------------
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>	// std::ostringstream
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+#include "rgAddrMap.h"
+#include "rgIoPins.h"
+
+
+//==========================================================================
+//## rgReg_rw::  Base Class
+//==========================================================================
+
+/*
+* Modify register.
+*    Does a read/modify/write for normal read-write capable registers.
+*/
+void
+rgReg_rw::modify(
+    uint32_t		mask,
+    uint32_t		value
+)
+{
+    uint32_t            x;
+
+    x = read();
+    x = (x & (~ mask) ) | (value & mask);
+    write( x );
+}
+
+
+//==========================================================================
+//## rgIoPins::  Main Class
+//==========================================================================
+
+/*
+* Constructor.
+* Require address map initialization.
+*    rgAddrMap	amx;		// address map object
+*    amx.open_dev_mem();	// select and open device file
+* call:
+*    rgIoPins	gx  ( &amx );	// constructor with address map
+*    &amx  = pointer to address map object with open device file
+*/
+rgIoPins::rgIoPins(
+    rgAddrMap		*xx
+)
+{
+    GpioBase     = xx->get_mem_block( FeatureAddr );
+
+    // Real Registers
+
+		 Fsel0.init_addr( GpioBase + (0x00 /4) );
+		 Fsel1.init_addr( GpioBase + (0x04 /4) );
+		 Fsel2.init_addr( GpioBase + (0x08 /4) );
+		 Fsel3.init_addr( GpioBase + (0x0c /4) );
+		 Fsel4.init_addr( GpioBase + (0x10 /4) );
+		 Fsel5.init_addr( GpioBase + (0x14 /4) );
+
+	     PinSet_w0.init_addr( GpioBase + (0x1c /4) );
+	     PinSet_w1.init_addr( GpioBase + (0x20 /4) );
+
+	     PinClr_w0.init_addr( GpioBase + (0x28 /4) );
+	     PinClr_w1.init_addr( GpioBase + (0x2c /4) );
+
+	    PinRead_w0.init_addr( GpioBase + (0x34 /4) );
+	    PinRead_w1.init_addr( GpioBase + (0x38 /4) );
+
+	EventStatus_w0.init_addr( GpioBase + (0x40 /4) );
+	EventStatus_w1.init_addr( GpioBase + (0x44 /4) );
+
+	 DetectRise_w0.init_addr( GpioBase + (0x4c /4) );
+	 DetectRise_w1.init_addr( GpioBase + (0x50 /4) );
+
+	 DetectFall_w0.init_addr( GpioBase + (0x58 /4) );
+	 DetectFall_w1.init_addr( GpioBase + (0x5c /4) );
+
+	 DetectHigh_w0.init_addr( GpioBase + (0x64 /4) );
+	 DetectHigh_w1.init_addr( GpioBase + (0x68 /4) );
+
+	  DetectLow_w0.init_addr( GpioBase + (0x70 /4) );
+	  DetectLow_w1.init_addr( GpioBase + (0x74 /4) );
+
+    DetectAsyncRise_w0.init_addr( GpioBase + (0x7c /4) );
+    DetectAsyncRise_w1.init_addr( GpioBase + (0x80 /4) );
+
+    DetectAsyncFall_w0.init_addr( GpioBase + (0x88 /4) );
+    DetectAsyncFall_w1.init_addr( GpioBase + (0x8c /4) );
+
+      PullUpDown.      init_addr( GpioBase + (0x94 /4) );
+      PullUpDownClk_w0.init_addr( GpioBase + (0x98 /4) );
+      PullUpDownClk_w1.init_addr( GpioBase + (0x9c /4) );
+
+    // Virtual Registers
+
+    PinLevel_w0.init_addr(     PinRead_w0.addr() );
+    PinLevel_w0.init_addr_set(  PinSet_w0.addr() );
+    PinLevel_w0.init_addr_clr(  PinClr_w0.addr() );
+
+    PinLevel_w1.init_addr(     PinRead_w1.addr() );
+    PinLevel_w1.init_addr_set(  PinSet_w1.addr() );
+    PinLevel_w1.init_addr_clr(  PinClr_w1.addr() );
+}
+
---
+++ librgpio/src/rgIoPins.h
@@ -0,0 +1,149 @@
+// 2020-04-19  William A. Hudson
+
+#ifndef rgIoPin_P
+#define rgIoPin_P
+
+//--------------------------------------------------------------------------
+// rGPIO  Io Pin Registers.
+//--------------------------------------------------------------------------
+
+class rgReg {		// Base class for any write-style.
+
+  protected:
+    volatile uint32_t	*Addr;		// register address
+
+  public:
+		// constructor
+			rgReg()				{ Addr = 0; }
+
+    inline
+    volatile uint32_t*	addr()				{ return Addr; }
+
+    inline uint32_t	read()				{ return *Addr; }
+
+    inline void		write( uint32_t rval )		{ *Addr = rval; }
+
+		// Initialization:  (private, used by feature constructor)
+  public:
+    inline void		init_addr( volatile uint32_t* aptr)  { Addr = aptr; }
+};
+
+
+class rgReg_set : public rgReg {	// set-only
+  public:
+    inline void		set( uint32_t rval )		{ *Addr = rval; }
+};
+
+
+class rgReg_clr : public rgReg {	// clear-only
+  public:
+    inline void		clr( uint32_t rval )		{ *Addr = rval; }
+};
+
+
+class rgReg_rw  : public rgReg {	// normal read-write
+  public:
+    inline void		set(    uint32_t mask )   { write( read() |  mask ); }
+    inline void		clr(    uint32_t mask )   { write( read() & ~mask ); }
+    void		modify( uint32_t mask,  uint32_t value );
+};
+
+
+//#!!  rgReg_3rw  generic virtual register
+class rgReg_PinLevel : public rgReg {	// virtual register
+  private:
+//  volatile uint32_t	*Addr;		// inherited
+    volatile uint32_t	*AddrSet;	// register address
+    volatile uint32_t	*AddrClr;	// register address
+
+  public:
+		// constructor
+			rgReg_PinLevel()	{ AddrSet = 0;  AddrClr= 0; }
+
+    inline
+    volatile uint32_t*	addr_set()			{ return AddrSet; }
+    volatile uint32_t*	addr_clr()			{ return AddrClr; }
+
+    inline void		set( uint32_t rval )		{ *AddrSet = rval; }
+    inline void		clr( uint32_t rval )		{ *AddrClr = rval; }
+
+    inline void		modify( uint32_t mask,  uint32_t rval ) {
+			    *AddrSet = (mask &  rval);
+			    *AddrClr = (mask & ~rval);
+			}
+
+		// Initialization:  (private, used by feature constructor)
+  public:
+    inline void		init_addr_set( volatile uint32_t* aptr)
+			    { AddrSet = aptr; }
+    inline void		init_addr_clr( volatile uint32_t* aptr)
+			    { AddrClr = aptr; }
+};
+
+
+class rgIoPins {
+  private:
+    volatile uint32_t	*GpioBase;	// IO base address
+
+  public:
+				// Virtual Registers
+    rgReg_PinLevel	PinLevel_w0;
+    rgReg_PinLevel	PinLevel_w1;
+
+				// Real Registers
+    rgReg_rw		Fsel0;
+    rgReg_rw		Fsel1;
+    rgReg_rw		Fsel2;
+    rgReg_rw		Fsel3;
+    rgReg_rw		Fsel4;
+    rgReg_rw		Fsel5;
+
+    rgReg_set		PinSet_w0;		// set-only, no read
+    rgReg_set		PinSet_w1;
+
+    rgReg_clr		PinClr_w0;		// clear-only, no read
+    rgReg_clr		PinClr_w1;
+
+    rgReg_rw		PinRead_w0;		// read-only
+    rgReg_rw		PinRead_w1;
+
+    rgReg_clr		EventStatus_w0;		// read/clear
+    rgReg_clr		EventStatus_w1;
+
+    rgReg_rw		DetectRise_w0;
+    rgReg_rw		DetectRise_w1;
+
+    rgReg_rw		DetectFall_w0;
+    rgReg_rw		DetectFall_w1;
+
+    rgReg_rw		DetectHigh_w0;
+    rgReg_rw		DetectHigh_w1;
+
+    rgReg_rw		DetectLow_w0;
+    rgReg_rw		DetectLow_w1;
+
+    rgReg_rw		DetectAsyncRise_w0;
+    rgReg_rw		DetectAsyncRise_w1;
+
+    rgReg_rw		DetectAsyncFall_w0;
+    rgReg_rw		DetectAsyncFall_w1;
+
+    rgReg_rw		PullUpDown;
+    rgReg_rw		PullUpDownClk_w0;
+    rgReg_rw		PullUpDownClk_w1;
+
+  private:
+    static const uint32_t	FeatureAddr  = 0x7e200000;	// BCM doc
+
+  public:
+    rgIoPins(			// constructor
+	rgAddrMap	*xx
+    );
+
+    inline
+    volatile uint32_t*	get_base_addr()		{ return  GpioBase; }
+    inline uint32_t	get_bcm_address()	{ return  FeatureAddr; }
+};
+
+#endif
+
