73377223ce1f97874ac43949b979b6a392ad00b0
William Hudson <wahudson>
2020-04-21 21:44:05 -0600

    Test:  t_io/  Revise for new rgpio-io using rgIoPins:: register objects.     No coverage change.

--- rgpio/test/t_io/test.pl
+++ rgpio/test/t_io/test.pl
@@ -48,22 +48,22 @@ run_test( "11", "io no args",
     Stderr => q(),
     Stdout => q(
 	0x00000000  PinLevel_w0
-	0x00000000  rgEventStatus_w0
-	0x00000000  rgDetectRising_w0
-	0x00000000  rgDetectFalling_w0
-	0x00000000  rgDetectHigh_w0
-	0x00000000  rgDetectLow_w0
-	0x00000000  rgDetectAsyncRising_w0
-	0x00000000  rgDetectAsyncFalling_w0
+	0x00000000  EventStatus_w0
+	0x00000000  DetectRise_w0
+	0x00000000  DetectFall_w0
+	0x00000000  DetectHigh_w0
+	0x00000000  DetectLow_w0
+	0x00000000  DetectAsyncRise_w0
+	0x00000000  DetectAsyncFall_w0
 
 	0x00000000  PinLevel_w1
-	0x00000000  rgEventStatus_w1
-	0x00000000  rgDetectRising_w1
-	0x00000000  rgDetectFalling_w1
-	0x00000000  rgDetectHigh_w1
-	0x00000000  rgDetectLow_w1
-	0x00000000  rgDetectAsyncRising_w1
-	0x00000000  rgDetectAsyncFalling_w1
+	0x00000000  EventStatus_w1
+	0x00000000  DetectRise_w1
+	0x00000000  DetectFall_w1
+	0x00000000  DetectHigh_w1
+	0x00000000  DetectLow_w1
+	0x00000000  DetectAsyncRise_w1
+	0x00000000  DetectAsyncFall_w1
     ),
 );
 
@@ -89,25 +89,25 @@ run_test( "13", "bad option",
 #---------------------------------------------------------------------------
 
 run_test( "20", "io --set",
-    "rgpio --dev=f  io --set=0xfff  rgDetectLow_w0",
+    "rgpio --dev=f  io --set=0xfff  DetectLow_w0",
     0,
     Stderr => q(),
     Stdout => q(
-	0x00000fff  rgDetectLow_w0
+	0x00000fff  DetectLow_w0
     ),
 );
 
 run_test( "20b", "io --clr",
-    "rgpio --dev=f  io --clr=0x033  rgDetectLow_w0",
+    "rgpio --dev=f  io --clr=0x033  DetectLow_w0",
     0,
     Stderr => q(),
     Stdout => q(
-	0x00000000  rgDetectLow_w0
+	0x00000000  DetectLow_w0
     ),
 );
 
 run_test( "21", "io --set",
-    "rgpio --dev=f  io --set=0xf --clr=0x3  rgDetectLow_w0",
+    "rgpio --dev=f  io --set=0xf --clr=0x3  DetectLow_w0",
     1,
     Stderr => q(
 	Error:  --set invalid with --clr or --mask and --value
@@ -117,7 +117,7 @@ run_test( "21", "io --set",
 );
 
 run_test( "22", "io --set",
-    "rgpio --dev=f  io --set=0xf --mask=0x3 --value=0x0  rgDetectLow_w0",
+    "rgpio --dev=f  io --set=0xf --mask=0x3 --value=0x0  DetectLow_w0",
     1,
     Stderr => q(
 	Error:  --set invalid with --clr or --mask and --value
@@ -126,7 +126,7 @@ run_test( "22", "io --set",
 );
 
 run_test( "23", "io --clr",
-    "rgpio --dev=f  io --clr=0xf --mask=0x3 --value=0x0  rgDetectLow_w0",
+    "rgpio --dev=f  io --clr=0xf --mask=0x3 --value=0x0  DetectLow_w0",
     1,
     Stderr => q(
 	Error:  --clr invalid with --set or --mask and --value
@@ -136,7 +136,7 @@ run_test( "23", "io --clr",
 
 #---------------------------------------
 run_test( "24a", "io --mask",
-    "rgpio --dev=f  io  --mask=0x3  rgDetectLow_w0",
+    "rgpio --dev=f  io  --mask=0x3  DetectLow_w0",
     1,
     Stderr => q(
 	Error:  modify requires --mask --value
@@ -145,7 +145,7 @@ run_test( "24a", "io --mask",
 );
 
 run_test( "24b", "io --value",
-    "rgpio --dev=f  io  --value=0x0  rgDetectLow_w0",
+    "rgpio --dev=f  io  --value=0x0  DetectLow_w0",
     1,
     Stderr => q(
 	Error:  modify requires --mask --value
@@ -168,10 +168,10 @@ run_test( "25b", "io modify invalid with reg groups",
     1,
     Stderr => q(
 	Error:  modification invalid with --w0 --w1 --fsel --pud --all
-	Error:  set only:  rgPinSet_w0
-	Error:  set only:  rgPinSet_w1
-	Error:  read only:  rgPinRead_w0
-	Error:  read only:  rgPinRead_w1
+	Error:  set only:  PinSet_w0
+	Error:  set only:  PinSet_w1
+	Error:  read only:  PinRead_w0
+	Error:  read only:  PinRead_w1
     ),
     Stdout => q(),
 );
@@ -181,7 +181,7 @@ run_test( "25c", "io modify invalid with reg groups",
     1,
     Stderr => q(
 	Error:  modification invalid with --w0 --w1 --fsel --pud --all
-	Error:  clear only:  rgEventStatus_w0
+	Error:  clear only:  EventStatus_w0
     ),
     Stdout => q(),
 );
@@ -204,27 +204,27 @@ run_test( "31", "io --raw instead of pseudo registers",
     0,
     Stderr => q(),
     Stdout => q(
-	0x00000000  rgPinSet_w0
-	0x00000000  rgPinClr_w0
-	0x00000000  rgPinRead_w0
-	0x00000000  rgEventStatus_w0
-	0x00000000  rgDetectRising_w0
-	0x00000000  rgDetectFalling_w0
-	0x00000000  rgDetectHigh_w0
-	0x00000000  rgDetectLow_w0
-	0x00000000  rgDetectAsyncRising_w0
-	0x00000000  rgDetectAsyncFalling_w0
+	0x00000000  PinSet_w0
+	0x00000000  PinClr_w0
+	0x00000000  PinRead_w0
+	0x00000000  EventStatus_w0
+	0x00000000  DetectRise_w0
+	0x00000000  DetectFall_w0
+	0x00000000  DetectHigh_w0
+	0x00000000  DetectLow_w0
+	0x00000000  DetectAsyncRise_w0
+	0x00000000  DetectAsyncFall_w0
 
-	0x00000000  rgPinSet_w1
-	0x00000000  rgPinClr_w1
-	0x00000000  rgPinRead_w1
-	0x00000000  rgEventStatus_w1
-	0x00000000  rgDetectRising_w1
-	0x00000000  rgDetectFalling_w1
-	0x00000000  rgDetectHigh_w1
-	0x00000000  rgDetectLow_w1
-	0x00000000  rgDetectAsyncRising_w1
-	0x00000000  rgDetectAsyncFalling_w1
+	0x00000000  PinSet_w1
+	0x00000000  PinClr_w1
+	0x00000000  PinRead_w1
+	0x00000000  EventStatus_w1
+	0x00000000  DetectRise_w1
+	0x00000000  DetectFall_w1
+	0x00000000  DetectHigh_w1
+	0x00000000  DetectLow_w1
+	0x00000000  DetectAsyncRise_w1
+	0x00000000  DetectAsyncFall_w1
     ),
 );
 
@@ -234,13 +234,13 @@ run_test( "32", "io --w0",
     Stderr => q(),
     Stdout => q(
 	0x00000000  PinLevel_w0
-	0x00000000  rgEventStatus_w0
-	0x00000000  rgDetectRising_w0
-	0x00000000  rgDetectFalling_w0
-	0x00000000  rgDetectHigh_w0
-	0x00000000  rgDetectLow_w0
-	0x00000000  rgDetectAsyncRising_w0
-	0x00000000  rgDetectAsyncFalling_w0
+	0x00000000  EventStatus_w0
+	0x00000000  DetectRise_w0
+	0x00000000  DetectFall_w0
+	0x00000000  DetectHigh_w0
+	0x00000000  DetectLow_w0
+	0x00000000  DetectAsyncRise_w0
+	0x00000000  DetectAsyncFall_w0
     ),
 );
 
@@ -249,12 +249,12 @@ run_test( "33", "io --fsel",
     0,
     Stderr => q(),
     Stdout => q(
-	0x00000000  rgFsel0
-	0x00000000  rgFsel1
-	0x00000000  rgFsel2
-	0x00000000  rgFsel3
-	0x00000000  rgFsel4
-	0x00000000  rgFsel5
+	0x00000000  Fsel0
+	0x00000000  Fsel1
+	0x00000000  Fsel2
+	0x00000000  Fsel3
+	0x00000000  Fsel4
+	0x00000000  Fsel5
     ),
 );
 
@@ -263,9 +263,9 @@ run_test( "34", "io --pud",
     0,
     Stderr => q(),
     Stdout => q(
-	0x00000000  rgPullUpDown
-	0x00000000  rgPullUpDownClk_w0
-	0x00000000  rgPullUpDownClk_w1
+	0x00000000  PullUpDown
+	0x00000000  PullUpDownClk_w0
+	0x00000000  PullUpDownClk_w1
     ),
 );
 
@@ -276,23 +276,23 @@ run_test( "35", "io --all -v  binary output",
 );
 
 run_test( "36", "io  register args",
-    "rgpio --dev=f  io  PinLevel_w0 rgDetectHigh_w1",
+    "rgpio --dev=f  io  PinLevel_w0 DetectHigh_w1",
     0,
     Stderr => q(),
     Stdout => q(
 	0x00000000  PinLevel_w0
-	0x00000000  rgDetectHigh_w1
+	0x00000000  DetectHigh_w1
     ),
 );
 
 run_test( "37", "io  register args",
-    "rgpio --dev=f  io -v --set=0x00ff5533  PinLevel_w0 rgDetectHigh_w1",
+    "rgpio --dev=f  io -v --set=0x00ff5533  PinLevel_w0 DetectHigh_w1",
     0,
     Stderr => q(),
     Stdout => q(
-	IO Pin Registers:                      28   24   20   16   12    8    4    0
-	0x00000000  PinLevel_w0              0000 0000 0000 0000 0000 0000 0000 0000
-	0x00ff5533  rgDetectHigh_w1          0000 0000 1111 1111 0101 0101 0011 0011
+	IO Pin Registers:                 28   24   20   16   12    8    4    0
+	0x00000000  PinLevel_w0         0000 0000 0000 0000 0000 0000 0000 0000
+	0x00ff5533  DetectHigh_w1       0000 0000 1111 1111 0101 0101 0011 0011
     ),
 );
 
