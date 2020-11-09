4daeb52788a40c05e194d71cf34e4bf5ce393982
William Hudson <wahudson>
2020-11-09 15:16:21 -0700

    rgAltFuncName:  Add sd0_*, sd1_* names for SD card.
    Names after CM3 Compute Module datasheet.

--- librgpio/src/rgAltFuncName.cpp
+++ librgpio/src/rgAltFuncName.cpp
@@ -38,7 +38,8 @@ rgAltFuncName::rgAltFuncName()
 * Alternate function name table.
 *     These names are adapted from the BCM documentation, modified to help
 *     see what feature it belongs to.
-*     Named set from BCM Doc "BCM2835 ARM Peripherals (06 February 2012)".
+*     Named set from BCM Doc "BCM2835 ARM Peripherals (06 February 2012)" and
+*     CM3 Compute Module Datasheet.
 *
 * AltFunc[altN][bit]
 *     altN  = Alternate function number 0..5 (for Alt0..Alt5).
@@ -72,12 +73,12 @@ const char*		rgAltFuncName::AltFunc[][6] = {
   { "pcm_FS",     "sm_D11", "--", "bsc_SCLK", "spi1_MISO",  "PWM1"    }, // 19
   { "pcm_DIN",    "sm_D12", "--", "bsc_MISO", "spi1_MOSI",  "gp_CLK0" }, // 20
   { "pcm_DOUT",   "sm_D13", "--", "bsc_CE_n", "spi1_SCLK",  "gp_CLK1" }, // 21
-  { "--",         "sm_D14", "--", "sd1_CLK",  "arm_TRST",   "**"      }, // 22
-  { "--",         "sm_D15", "--", "sd1_CMD",  "arm_RTCK",   "**"      }, // 23
-  { "--",         "sm_D16", "--", "sd1_DAT0", "arm_TDO",    "**"      }, // 24
-  { "--",         "sm_D17", "--", "sd1_DAT1", "arm_TCK",    "**"      }, // 25
-  { "--",         "--",    "--",  "sd1_DAT2", "arm_TDI",    "**"      }, // 26
-  { "--",         "--",    "--",  "sd1_DAT3", "arm_TMS",    "**"      }, // 27
+  { "sd0_CLK",    "sm_D14", "--", "sd1_CLK",  "arm_TRST",   "**"      }, // 22
+  { "sd0_CMD",    "sm_D15", "--", "sd1_CMD",  "arm_RTCK",   "**"      }, // 23
+  { "sd0_DAT0",   "sm_D16", "--", "sd1_DAT0", "arm_TDO",    "**"      }, // 24
+  { "sd0_DAT1",   "sm_D17", "--", "sd1_DAT1", "arm_TCK",    "**"      }, // 25
+  { "sd0_DAT2",   "--",     "--", "sd1_DAT2", "arm_TDI",    "**"      }, // 26
+  { "sd0_DAT3",   "--",     "--", "sd1_DAT3", "arm_TMS",    "**"      }, // 27
 
   { "iic0_SDA",   "sm_A5", "pcm_CLK",  "--",     "**",      "**"      }, // 28
   { "iic0_SCL",   "sm_A4", "pcm_FS",   "--",     "**",      "**"      }, // 29
@@ -85,18 +86,18 @@ const char*		rgAltFuncName::AltFunc[][6] = {
   { "--",         "sm_A2", "pcm_DOUT", "u0_RTS", "**",      "u1_RTS"  }, // 31
   { "gp_CLK0",    "sm_A1", "--",       "u0_TXD", "**",      "u1_TXD"  }, // 32
   { "--" ,        "sm_A0", "--",       "u0_RXD", "**",      "u1_RXD"  }, // 33
-  { "gp_CLK0",    "sm_OE_n", "--",     "--",     "**",      "**"      }, // 34
-  { "spi0_CE1_n", "sm_WE_n", "**",     "--",     "**",      "**"      }, // 35
-  { "spi0_CE0_n", "sm_D0", "u0_TXD",   "--",     "**",      "**"      }, // 36
-  { "spi0_MISO",  "sm_D1", "u0_RXD",   "--",     "**",      "**"      }, // 37
-  { "spi0_MOSI",  "sm_D2", "u0_RTS",   "--",     "**",      "**"      }, // 38
-  { "spi0_SCLK",  "sm_D3", "u0_CTS",   "--",     "**",      "**"      }, // 39
-  { "PWM0",       "sm_D4", "**",       "--",  "spi2_MISO",  "u1_TXD"  }, // 40
-  { "PWM1",       "sm_D5", "--",       "--",  "spi2_MOSI",  "u1_RXD"  }, // 41
-  { "gp_CLK1",    "sm_D6", "--",       "--",  "spi2_SCLK",  "u1_RTS"  }, // 42
-  { "gp_CLK2",    "sm_D7", "--",       "--",  "spi2_CE0_n", "u1_CTS"  }, // 43
-  { "gp_CLK1", "iic0_SDA", "iic1_SDA", "--",  "spi2_CE1_n", "**"      }, // 44
-  { "PWM1",    "iic0_SCL", "iic1_SCL", "--",  "spi2_CE2_n", "**"      }, // 45
+  { "gp_CLK0",    "sm_OE_n", "--",   "sd1_CLK",  "**",      "**"      }, // 34
+  { "spi0_CE1_n", "sm_WE_n", "**",   "sd1_CMD",  "**",      "**"      }, // 35
+  { "spi0_CE0_n", "sm_D0", "u0_TXD", "sd1_DAT0", "**",      "**"      }, // 36
+  { "spi0_MISO",  "sm_D1", "u0_RXD", "sd1_DAT1", "**",      "**"      }, // 37
+  { "spi0_MOSI",  "sm_D2", "u0_RTS", "sd1_DAT2", "**",      "**"      }, // 38
+  { "spi0_SCLK",  "sm_D3", "u0_CTS", "sd1_DAT3", "**",      "**"      }, // 39
+  { "PWM0",       "sm_D4", "**",     "sd1_DAT4", "spi2_MISO",  "u1_TXD" },// 40
+  { "PWM1",       "sm_D5", "--",     "sd1_DAT5", "spi2_MOSI",  "u1_RXD" },// 41
+  { "gp_CLK1",    "sm_D6", "--",     "sd1_DAT6", "spi2_SCLK",  "u1_RTS" },// 42
+  { "gp_CLK2",    "sm_D7", "--",     "sd1_DAT7", "spi2_CE0_n", "u1_CTS" },// 43
+  { "gp_CLK1", "iic0_SDA", "iic1_SDA", "--",     "spi2_CE1_n", "**"     },// 44
+  { "PWM1",    "iic0_SCL", "iic1_SCL", "--",     "spi2_CE2_n", "**"     },// 45
 
   { "--",         "**",    "**",       "**",     "**",      "**"      }, // 46
   { "--",         "**",    "**",       "**",     "**",      "**"      }, // 47
