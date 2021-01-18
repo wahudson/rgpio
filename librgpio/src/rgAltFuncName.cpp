19f381eac4f005cd14d003fd56debf3744fdd108
William Hudson <wahudson>
2021-01-18 12:05:56 -0700

    rgAltFuncName:  Adjust whitespace in AltFunc[][6] name table.
    No functional change (diff -w).
    Adjusted to fit 80-column window, because I like that.

--- librgpio/src/rgAltFuncName.cpp
+++ librgpio/src/rgAltFuncName.cpp
@@ -53,64 +53,64 @@ rgAltFuncName::rgAltFuncName()
 *     **    = do NOT USE, may have unexpected results
 */
 const char*		rgAltFuncName::AltFunc[][6] = {
- // Alt0          Alt1       Alt2         Alt3     Alt4     Alt5           bit
+ // Alt0          Alt1       Alt2        Alt3     Alt4       Alt5          bit
 
-  { "iic0_SDA",   "sm_A5",   "dpi_PCLK",  "**",    "**",    "**"       }, // 0
-  { "iic0_SCL",   "sm_A4",   "dpi_DE",    "**",    "**",    "**"       }, // 1
-  { "iic1_SDA",   "sm_A3",   "dpi_VSYNC", "**",    "**",    "**"       }, // 2
-  { "iic1_SCL",   "sm_A2",   "dpi_HSYNC", "**",    "**",    "**"       }, // 3
-  { "gp_CLK0",    "sm_A1",   "dpi_D0",    "**",    "**",    "arm_TDI"  }, // 4
-  { "gp_CLK1",    "sm_A0",   "dpi_D1",    "**",    "**",    "arm_TDO"  }, // 5
-  { "gp_CLK2",    "sm_OE_n", "dpi_D2",    "**",    "**",    "arm_RTCK" }, // 6
-  { "spi0_CE1_n", "sm_WE_n", "dpi_D3",    "**",    "**",    "**"       }, // 7
-  { "spi0_CE0_n", "sm_D0",  "dpi_D4",     "**",    "**",    "**"       }, // 8
-  { "spi0_MISO",  "sm_D1",  "dpi_D5",     "**",    "**",    "**"       }, // 9
-  { "spi0_MOSI",  "sm_D2",  "dpi_D6",     "**",    "**",    "**"       }, // 10
-  { "spi0_SCLK",  "sm_D3",  "dpi_D7",     "**",    "**",    "**"       }, // 11
-  { "PWM0",       "sm_D4",  "dpi_D8",     "**",    "**",    "arm_TMS"  }, // 12
-  { "PWM1",       "sm_D5",  "dpi_D9",     "**",    "**",    "arm_TCK"  }, // 13
-  { "u0_TXD",     "sm_D6",  "dpi_D10",    "**",    "**",         "u1_TXD"  }, // 14
-  { "u0_RXD",     "sm_D7",  "dpi_D11",    "**",    "**",         "u1_RXD"  }, // 15
-  { "--",         "sm_D8",  "dpi_D12", "u0_CTS",   "spi1_CE2_n", "u1_CTS"  }, // 16
-  { "--",         "sm_D9",  "dpi_D13", "u0_RTS",   "spi1_CE1_n", "u1_RTS"  }, // 17
-  { "pcm_CLK",    "sm_D10", "dpi_D14", "bsc_MOSI", "spi1_CE0_n", "PWM0"    }, // 18
-  { "pcm_FS",     "sm_D11", "dpi_D15", "bsc_SCLK", "spi1_MISO",  "PWM1"    }, // 19
-  { "pcm_DIN",    "sm_D12", "dpi_D16", "bsc_MISO", "spi1_MOSI",  "gp_CLK0" }, // 20
-  { "pcm_DOUT",   "sm_D13", "dpi_D17", "bsc_CE_n", "spi1_SCLK",  "gp_CLK1" }, // 21
-  { "sd0_CLK",    "sm_D14", "dpi_D18", "sd1_CLK",  "arm_TRST",   "**"  }, // 22
-  { "sd0_CMD",    "sm_D15", "dpi_D19", "sd1_CMD",  "arm_RTCK",   "**"  }, // 23
-  { "sd0_DAT0",   "sm_D16", "dpi_D20", "sd1_DAT0", "arm_TDO",    "**"  }, // 24
-  { "sd0_DAT1",   "sm_D17", "dpi_D21", "sd1_DAT1", "arm_TCK",    "**"  }, // 25
-  { "sd0_DAT2",   "--",     "dpi_D22", "sd1_DAT2", "arm_TDI",    "**"  }, // 26
-  { "sd0_DAT3",   "--",     "dpi_D23", "sd1_DAT3", "arm_TMS",    "**"  }, // 27
+ { "iic0_SDA",   "sm_A5",   "dpi_PCLK",  "**",    "**",      "**"       },// 0
+ { "iic0_SCL",   "sm_A4",   "dpi_DE",    "**",    "**",      "**"       },// 1
+ { "iic1_SDA",   "sm_A3",   "dpi_VSYNC", "**",    "**",      "**"       },// 2
+ { "iic1_SCL",   "sm_A2",   "dpi_HSYNC", "**",    "**",      "**"       },// 3
+ { "gp_CLK0",    "sm_A1",   "dpi_D0",    "**",    "**",      "arm_TDI"  },// 4
+ { "gp_CLK1",    "sm_A0",   "dpi_D1",    "**",    "**",      "arm_TDO"  },// 5
+ { "gp_CLK2",    "sm_OE_n", "dpi_D2",    "**",    "**",      "arm_RTCK" },// 6
+ { "spi0_CE1_n", "sm_WE_n", "dpi_D3",    "**",    "**",      "**"       },// 7
+ { "spi0_CE0_n", "sm_D0",   "dpi_D4",    "**",    "**",      "**"       },// 8
+ { "spi0_MISO",  "sm_D1",   "dpi_D5",    "**",    "**",      "**"       },// 9
+ { "spi0_MOSI",  "sm_D2",   "dpi_D6",    "**",    "**",      "**"       },// 10
+ { "spi0_SCLK",  "sm_D3",   "dpi_D7",    "**",    "**",      "**"       },// 11
+ { "PWM0",       "sm_D4",   "dpi_D8",    "**",    "**",      "arm_TMS"  },// 12
+ { "PWM1",       "sm_D5",   "dpi_D9",    "**",    "**",      "arm_TCK"  },// 13
+ { "u0_TXD",     "sm_D6",  "dpi_D10",    "**",    "**",         "u1_TXD"  },//14
+ { "u0_RXD",     "sm_D7",  "dpi_D11",    "**",    "**",         "u1_RXD"  },//15
+ { "--",         "sm_D8",  "dpi_D12", "u0_CTS",   "spi1_CE2_n", "u1_CTS"  },//16
+ { "--",         "sm_D9",  "dpi_D13", "u0_RTS",   "spi1_CE1_n", "u1_RTS"  },//17
+ { "pcm_CLK",    "sm_D10", "dpi_D14", "bsc_MOSI", "spi1_CE0_n", "PWM0"    },//18
+ { "pcm_FS",     "sm_D11", "dpi_D15", "bsc_SCLK", "spi1_MISO",  "PWM1"    },//19
+ { "pcm_DIN",    "sm_D12", "dpi_D16", "bsc_MISO", "spi1_MOSI",  "gp_CLK0" },//20
+ { "pcm_DOUT",   "sm_D13", "dpi_D17", "bsc_CE_n", "spi1_SCLK",  "gp_CLK1" },//21
+ { "sd0_CLK",    "sm_D14", "dpi_D18", "sd1_CLK",  "arm_TRST",   "**"    },// 22
+ { "sd0_CMD",    "sm_D15", "dpi_D19", "sd1_CMD",  "arm_RTCK",   "**"    },// 23
+ { "sd0_DAT0",   "sm_D16", "dpi_D20", "sd1_DAT0", "arm_TDO",    "**"    },// 24
+ { "sd0_DAT1",   "sm_D17", "dpi_D21", "sd1_DAT1", "arm_TCK",    "**"    },// 25
+ { "sd0_DAT2",   "--",     "dpi_D22", "sd1_DAT2", "arm_TDI",    "**"    },// 26
+ { "sd0_DAT3",   "--",     "dpi_D23", "sd1_DAT3", "arm_TMS",    "**"    },// 27
 
-  { "iic0_SDA",   "sm_A5", "pcm_CLK",  "--",     "**",      "**"      }, // 28
-  { "iic0_SCL",   "sm_A4", "pcm_FS",   "--",     "**",      "**"      }, // 29
-  { "--",         "sm_A3", "pcm_DIN",  "u0_CTS", "**",      "u1_CTS"  }, // 30
-  { "--",         "sm_A2", "pcm_DOUT", "u0_RTS", "**",      "u1_RTS"  }, // 31
-  { "gp_CLK0",    "sm_A1", "--",       "u0_TXD", "**",      "u1_TXD"  }, // 32
-  { "--" ,        "sm_A0", "--",       "u0_RXD", "**",      "u1_RXD"  }, // 33
-  { "gp_CLK0",    "sm_OE_n", "--",   "sd1_CLK",  "**",      "**"      }, // 34
-  { "spi0_CE1_n", "sm_WE_n", "**",   "sd1_CMD",  "**",      "**"      }, // 35
-  { "spi0_CE0_n", "sm_D0", "u0_TXD", "sd1_DAT0", "**",      "**"      }, // 36
-  { "spi0_MISO",  "sm_D1", "u0_RXD", "sd1_DAT1", "**",      "**"      }, // 37
-  { "spi0_MOSI",  "sm_D2", "u0_RTS", "sd1_DAT2", "**",      "**"      }, // 38
-  { "spi0_SCLK",  "sm_D3", "u0_CTS", "sd1_DAT3", "**",      "**"      }, // 39
-  { "PWM0",       "sm_D4", "**",     "sd1_DAT4", "spi2_MISO",  "u1_TXD" },// 40
-  { "PWM1",       "sm_D5", "--",     "sd1_DAT5", "spi2_MOSI",  "u1_RXD" },// 41
-  { "gp_CLK1",    "sm_D6", "--",     "sd1_DAT6", "spi2_SCLK",  "u1_RTS" },// 42
-  { "gp_CLK2",    "sm_D7", "--",     "sd1_DAT7", "spi2_CE0_n", "u1_CTS" },// 43
-  { "gp_CLK1", "iic0_SDA", "iic1_SDA", "--",     "spi2_CE1_n", "**"     },// 44
-  { "PWM1",    "iic0_SCL", "iic1_SCL", "--",     "spi2_CE2_n", "**"     },// 45
+ { "iic0_SDA",   "sm_A5",  "pcm_CLK",  "--",       "**",       "**"     },// 28
+ { "iic0_SCL",   "sm_A4",  "pcm_FS",   "--",       "**",       "**"     },// 29
+ { "--",         "sm_A3",  "pcm_DIN",  "u0_CTS",   "**",       "u1_CTS" },// 30
+ { "--",         "sm_A2",  "pcm_DOUT", "u0_RTS",   "**",       "u1_RTS" },// 31
+ { "gp_CLK0",    "sm_A1",    "--",     "u0_TXD",   "**",       "u1_TXD" },// 32
+ { "--" ,        "sm_A0",    "--",     "u0_RXD",   "**",       "u1_RXD" },// 33
+ { "gp_CLK0",    "sm_OE_n",  "--",   "sd1_CLK",    "**",       "**"     },// 34
+ { "spi0_CE1_n", "sm_WE_n",  "**",   "sd1_CMD",    "**",       "**"     },// 35
+ { "spi0_CE0_n", "sm_D0",  "u0_TXD", "sd1_DAT0",   "**",       "**"     },// 36
+ { "spi0_MISO",  "sm_D1",  "u0_RXD", "sd1_DAT1",   "**",       "**"     },// 37
+ { "spi0_MOSI",  "sm_D2",  "u0_RTS", "sd1_DAT2",   "**",       "**"     },// 38
+ { "spi0_SCLK",  "sm_D3",  "u0_CTS", "sd1_DAT3",   "**",       "**"     },// 39
+ { "PWM0",       "sm_D4",  "**",     "sd1_DAT4", "spi2_MISO",  "u1_TXD" },// 40
+ { "PWM1",       "sm_D5",  "--",     "sd1_DAT5", "spi2_MOSI",  "u1_RXD" },// 41
+ { "gp_CLK1",    "sm_D6",  "--",     "sd1_DAT6", "spi2_SCLK",  "u1_RTS" },// 42
+ { "gp_CLK2",    "sm_D7",  "--",     "sd1_DAT7", "spi2_CE0_n", "u1_CTS" },// 43
+ { "gp_CLK1",  "iic0_SDA", "iic1_SDA",  "--",    "spi2_CE1_n", "**"     },// 44
+ { "PWM1",     "iic0_SCL", "iic1_SCL",  "--",    "spi2_CE2_n", "**"     },// 45
 
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 46
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 47
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 48
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 49
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 50
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 51
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 52
-  { "--",         "**",    "**",       "**",     "**",      "**"      }, // 53
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 46
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 47
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 48
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 49
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 50
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 51
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 52
+ { "--",         "**",     "**",        "**",      "**",       "**"     },// 53
 };
 
 
